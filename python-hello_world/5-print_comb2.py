cd0104f04d49f416098b6e7d3deec4b193d50ee4
MahiSoft95 <maheshs@brightcomgroup.com>
2022-12-27 17:39:14 +0000

    hellooo

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for k in range(100):
     if k == 99:
-        print('{:02d}'.format(k), end='')
+        print('{}'.format(99))
     else:
         print('{:02d}, '.format(k), end='')
